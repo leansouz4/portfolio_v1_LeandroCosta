b42b217d6cacfe70264a59ad1b22367791a96850
Leandro Souza <Leandrosouzag2@gmail.com>
2024-10-08 18:58:26 -0700

    Feat

---
+++ login/js/firebase.js
@@ -0,0 +1,25 @@
+/*
+  CCTB Website Development
+  IST105
+  Oct 2024
+  Description: This is a simple login website where students are asked to 
+  implement Social Network Login with Firebase
+*/
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDBzy0wK4GmSCk81VTZYJ9zSywM4a72Qaw",
+  authDomain: "portfoliov1leandrocosta.firebaseapp.com",
+  projectId: "portfoliov1leandrocosta",
+  storageBucket: "portfoliov1leandrocosta.appspot.com",
+  messagingSenderId: "676025741965",
+  appId: "1:676025741965:web:91ede94df06be2d1b0a8c4",
+  measurementId: "G-HBHQQ6ZGQ5"
+};
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
